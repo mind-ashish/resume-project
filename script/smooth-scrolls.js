e1ef3c1fc3b1c5b3ec25f66ddc192bb16f08003b
Aashish <aashish01official@gmail.com>
2019-06-15 22:34:27 +0530

    enclosing in IIFE

--- script/smooth-scrolls.js
+++ script/smooth-scrolls.js
@@ -1,34 +1,36 @@
-var anchorTags=document.querySelectorAll("#header-list a");
-for (var i=0;i<anchorTags.length;i++){
-    anchorTags[i].addEventListener('click',function(event){
-        event.preventDefault();
-        //prevent default to stop default behavior of anchor tags
-    
-        var targetSectionId= this.textContent.trim().toLowerCase();
+(function(){
+    var anchorTags=document.querySelectorAll("#header-list a");
+    for (var i=0;i<anchorTags.length;i++){
+        anchorTags[i].addEventListener('click',function(event){
+            event.preventDefault();
+            //prevent default to stop default behavior of anchor tags
         
-        var str=targetSectionId;
-        var targetDiv=document.getElementById(str);
-
-        //scroller
-        var targetDivCoordinates=targetDiv.getBoundingClientRect();
-        var id=setInterval(function(){
-
-            if(targetDivCoordinates.top<=0){
-                
-                clearInterval(id);
-                return;
-            }
-
-            if(str=="contact" && targetDivCoordinates.top<=316){
-                clearInterval(id);
-                return;
-            }
-
-           
-            window.scrollBy(0,50);  
-            targetDivCoordinates=targetDiv.getBoundingClientRect();
-           
-
-        },20); 
-    });
-}
+            var targetSectionId= this.textContent.trim().toLowerCase();
+            
+            var str=targetSectionId;
+            var targetDiv=document.getElementById(str);
+    
+            //scroller
+            var targetDivCoordinates=targetDiv.getBoundingClientRect();
+            var id=setInterval(function(){
+    
+                if(targetDivCoordinates.top<=0){
+                    
+                    clearInterval(id);
+                    return;
+                }
+    
+                if(str=="contact" && targetDivCoordinates.top<=316){
+                    clearInterval(id);
+                    return;
+                }
+    
+               
+                window.scrollBy(0,50);  
+                targetDivCoordinates=targetDiv.getBoundingClientRect();
+               
+    
+            },20); 
+        });
+    }
+})();
