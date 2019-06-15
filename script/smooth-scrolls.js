4d5ad6d990a0a632934a5317bfe24e9686b26701
Aashish <aashish01official@gmail.com>
2019-06-15 22:31:44 +0530

    adds animations

---
+++ script/smooth-scrolls.js
@@ -0,0 +1,34 @@
+var anchorTags=document.querySelectorAll("#header-list a");
+for (var i=0;i<anchorTags.length;i++){
+    anchorTags[i].addEventListener('click',function(event){
+        event.preventDefault();
+        //prevent default to stop default behavior of anchor tags
+    
+        var targetSectionId= this.textContent.trim().toLowerCase();
+        
+        var str=targetSectionId;
+        var targetDiv=document.getElementById(str);
+
+        //scroller
+        var targetDivCoordinates=targetDiv.getBoundingClientRect();
+        var id=setInterval(function(){
+
+            if(targetDivCoordinates.top<=0){
+                
+                clearInterval(id);
+                return;
+            }
+
+            if(str=="contact" && targetDivCoordinates.top<=316){
+                clearInterval(id);
+                return;
+            }
+
+           
+            window.scrollBy(0,50);  
+            targetDivCoordinates=targetDiv.getBoundingClientRect();
+           
+
+        },20); 
+    });
+}
