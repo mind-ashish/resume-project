e0f0fdfaf8e7b1438d4b89e37b18f731fccd8894
Aashish <aashish01official@gmail.com>
2019-06-16 00:27:13 +0530

    resolved merge conflict

--- script/smooth-scrolls.js
+++ script/smooth-scrolls.js
@@ -1,39 +1,4 @@
-<<<<<<< HEAD
-var anchorTags=document.querySelectorAll("#header-list a");
-for (var i=0;i<anchorTags.length;i++){
-    anchorTags[i].addEventListener('click',function(event){
-        event.preventDefault();
-        //prevent default to stop default behavior of anchor tags
-    
-        var targetSectionId= this.textContent.trim().toLowerCase();
-        
-        var str=targetSectionId;
-        var targetDiv=document.getElementById(str);
 
-        //scroller
-        var targetDivCoordinates=targetDiv.getBoundingClientRect();
-        var id=setInterval(function(){
-
-            if(targetDivCoordinates.top<=0){
-                
-                clearInterval(id);
-                return;
-            }
-
-            if(str=="contact" && targetDivCoordinates.top<=316){
-                clearInterval(id);
-                return;
-            }
-
-           
-            window.scrollBy(0,50);  
-            targetDivCoordinates=targetDiv.getBoundingClientRect();
-           
-
-        },20); 
-    });
-}
-=======
 (function(){
     var anchorTags=document.querySelectorAll("#header-list a");
     for (var i=0;i<anchorTags.length;i++){
@@ -70,4 +35,4 @@ for (var i=0;i<anchorTags.length;i++){
         });
     }
 })();
->>>>>>> animations
+
